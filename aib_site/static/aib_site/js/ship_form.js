c0f63c215498562d1daf208da76dd2de578d7c95
Cory Pride <Cp@lc101.local>
2022-08-11 16:56:11 -0500

    added the code to hide and make visible ship info

--- aib_site/static/aib_site/js/ship_form.js
+++ aib_site/static/aib_site/js/ship_form.js
@@ -1,34 +1,34 @@
-
+// prevValue is a variable that will be used to keep track of the id number of the div of the ship that was previously made visible
+let prevValue = 0;
 
 window.onload = function () {
     // write the code that changes a picture frame to match any choice in the ship_selector option
-   
 
     let shipSelector = document.getElementById("ship_selector");
-    // These two lines of code are to display/set the img window and related info after the user selects a ship
 
+    //this function is passed the id value of the ship that the user selects in order to make visible the ship image and its related items
+    showShipInfo(shipSelector.value);
 
-   
-    let divIdNum = shipSelector.value;
-    showShipInfo(divIdNum);
-  
-   shipSelector.addEventListener("change" ,function(){
-    
-    let divIdNum = shipSelector.value;
-    showShipInfo(divIdNum);
-   });
+    shipSelector.addEventListener("change", function () {
+        showShipInfo(shipSelector.value);
+    });
 
 
 }
 
 
 function showShipInfo(divIdNum) {
-    //    todo: use the querySelector to find all elements in the 'infoDivs' class and set each of their style.visibility ="hidden" first
     
-    let theDivIdstr = "div-"+divIdNum;
+
+    if (prevValue > 0) {
+        document.getElementById("div-" + prevValue).style.visibility = "hidden";
+    }
+    
+    prevValue = divIdNum;
+    let theDivIdstr = "div-" + divIdNum;
     let theDiv = document.getElementById(theDivIdstr);
     console.log(theDiv)
-    theDiv.style.visibility="visible";
+    theDiv.style.visibility = "visible";
 
 
 }

04289cbb0e19105e0a3171eb43a3ada81175d1a6
Cory Pride <Cp@lc101.local>
2022-08-14 01:39:57 -0500

    modified ship_form() function cleaned up the code

--- aib_site/forms.py
+++ aib_site/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from .models import Ship
 
-class ShipForm():
-    class Meta:
-        fields = ['shipId']
-        labels = {'shipId':'Ship Name:'}
+# class ShipForm():
+#     class Meta:
+#         fields = ['shipId']
+#         labels = {'shipId':'Ship Name:'}
--- aib_site/views.py
+++ aib_site/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django import forms
 
 from .models import Ship
-from .forms import ShipForm
+# from .forms import ShipForm
 
 # Create your views here.
 def index(request):
@@ -19,12 +19,9 @@ def all_ships(request):
 
 def ship_form(request):
     """This page displays a form that allows users to select the ship they want to use in a customized version of Alien Invasion."""
-    if request.method != 'POST':
-        # No data submitted; display the Choose form
-        form = ShipForm()
-    else:
-        # POST data submitted; process data
-        # data will be stored in a json file to be retrieved later and used for the customized alien invasion game
+    if request.method == 'POST':
+    # POST data submitted; process data
+    # data will be stored in a json file to be retrieved later and used for the customized alien invasion game
         filenameAndPath = "aib_site/static/aib_site/json/shipChoice.json"
         with open (filenameAndPath,"w") as f:
             shipId = {"shipId":int(request.POST["shipId"])}
@@ -32,7 +29,8 @@ def ship_form(request):
             f.close()
             # eventually there will be another page to show the addition of the ship of your choosing to code. And there will be an explanation to go with it
             return redirect("aib_site:all_ships")
-        
+
+    # No data submitted; display the Choose form
     ships = Ship.objects.all()
     context = {"ships":ships}
     return render(request,'aib_site/ship_form.html',context)

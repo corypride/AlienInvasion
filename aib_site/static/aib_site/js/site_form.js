f342b40cf0e386463e3ff83ee39256281fa8f916
Cory Pride <Cp@lc101.local>
2022-09-23 03:19:06 -0500

    added the screenSize select to the battlesite portfolio

--- aib_site/static/aib_site/js/site_form.js
+++ aib_site/static/aib_site/js/site_form.js
@@ -1,7 +1,18 @@
-/** This function takes the id and the sites's name from the item you selected and sets its value to a form that is attached to a modal. Your choice will be saved if you so choose or cancled otherwise.*/
-function populateForm(siteName,siteDisplayName){
+/** This function takes the id and uses it to grab the screen size as well as the sites's name from the item you selected and sets its value to a form that is attached to a modal. Your choice will be saved if you so choose or cancled otherwise.*/
+function populateForm(siteName,siteDisplayName,siteId){
 
-    document.getElementById("formInput").value = siteName;
-    document.getElementById("formText").innerText = `Your choice of ${siteDisplayName} will be submitted now.`;
+    let temp = "screenSize"+siteId;
+    let arr = document.getElementById(temp).value.split("X");
+    let width = arr[0].trim();
+    let height = arr[1].trim();
+
+    document.getElementById("siteInput").value = siteName;
+    document.getElementById("widthInput").value = width;
+    document.getElementById("heightInput").value = height;
+    
+    
+    document.getElementById("formText").innerText = `Your choice of ${siteDisplayName}, with the screen size set at ${width} X ${height} will be submitted now.`;
     
 }
+
+
